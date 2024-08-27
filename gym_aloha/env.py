e9f8c9f87b686591465a1190d2920aa53d340ed4
Stephen Mander <37083603+st7ma784@users.noreply.github.com>
2024-08-27 20:09:42 +0100

    Update env.py

--- gym_aloha/env.py
+++ gym_aloha/env.py
@@ -133,7 +133,7 @@ class AlohaEnv(gym.Env):
             physics = mujoco.Physics.from_xml_path(str(xml_path))
             task = InsertionEndEffectorTask()
         else:
-            xml_path=random.choice(["bimanual_viperx_transfer_cube.xml", "bimanual_viperx_insertion.xml", "bimanual_viperx_end_effector_transfer_cube.xml","bimanual_viperx_end_effector_insertion.xml"])
+            xml_path=ASSETS_DIR/"bimanual_viperx_transfer_cube.xml"#, "bimanual_viperx_insertion.xml", "bimanual_viperx_end_effector_transfer_cube.xml","bimanual_viperx_end_effector_insertion.xml"])
             physics = mujoco.Physics.from_xml_path(str(xml_path))
             task=PromptTask(self.desc)
         
